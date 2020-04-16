7fae76e19f9984191863a56a282bde8ec70eb4bd
Felix Moreno <felixmorenot18@hotmail.com>
2020-04-16 10:03:26 -0500

    Init Rest API - Person

---
+++ src/main/java/com/rest/service/RestPersonApiApplication.java
@@ -0,0 +1,13 @@
+package com.rest.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPersonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestPersonApiApplication.class, args);
+	}
+
+}
